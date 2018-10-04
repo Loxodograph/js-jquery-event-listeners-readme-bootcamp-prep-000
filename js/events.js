f96d2fe28a56dee66ccd38985531216c273e129e
Nick Olas <paldino.nicholas@gmail.com>
2018-10-04 21:01:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
-
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $('img').on("load", function(){
+    $(this).addClass("tasty");
+    $(this).css('border', "solid 2px red");
+  });
+}
+function pressIt(){
+  $('#typing').on("keydown", function(key){
+    if(key.which === 13){
+      alert('G was pressed');
+      return;
+    }
+    return;
+  });
+}
+function submitIt(){
+  $('#submit').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 $(document).ready(function(){
 
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
